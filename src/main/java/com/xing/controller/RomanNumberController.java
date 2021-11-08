33db8a3ee321acba6bcc677d3d89f8ae6443ee70
Xingxing Li <xingxingli2017@gmail.com>
2021-11-08 00:37:35 -0800

    Add Log info

--- src/main/java/com/xing/controller/RomanNumberController.java
+++ src/main/java/com/xing/controller/RomanNumberController.java
@@ -3,6 +3,7 @@ package com.xing.controller;
 import com.xing.entity.Result;
 import com.xing.service.RomanNumberService;
 import com.xing.util.exception.InvalidNumberException;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,12 +18,18 @@ public class RomanNumberController {
     @Autowired
     RomanNumberService service;
 
+    @Autowired
+    Logger logger;
+
     @GetMapping("/romannumeral")
     public Result getRamanNumber(@RequestParam("query") Integer number) throws InvalidNumberException {
-        System.out.println(number);
+//        System.out.println(number);
+        logger.debug(String.format("Input query = %d", number));
         Map<String, Object> map = new HashMap<>();
         map.put("input", number);
-        map.put("output", service.getRomanNumber(number));
+        String output = service.getRomanNumber(number);
+        logger.debug(String.format("Output string = %s", output));
+        map.put("output", output);
         return new Result(true, 200, "Success", map);
     }
 }
